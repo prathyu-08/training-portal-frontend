ea64cecb58e31d380467232853011133a08d554f
Bhumireddysusmita <bhumireddysusmita@gmail.com>
2026-03-06 21:06:36 +0530

    commit

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,15 +1,11 @@
 import UserDashboard from "./UserDashboard";
 import AdminDashboard from "./AdminDashboard";
-
 const Dashboard = () => {
   const role = localStorage.getItem("role");
-
   if (role === "admin") {
     return <AdminDashboard />;
   }
-
   // default → user
   return <UserDashboard />;
 };
-
 export default Dashboard;
